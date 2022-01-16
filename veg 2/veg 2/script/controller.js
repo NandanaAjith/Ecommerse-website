9b39a00f24d7149cd32dbf16e47cc0e4766b2770
Nandana Ajith <97352643+NandanaAjith@users.noreply.github.com>
2022-01-16 21:10:36 +0530

    Update controller.js

--- veg 2/veg 2/script/controller.js
+++ veg 2/veg 2/script/controller.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'index.html'
+            templateUrl: 'home.html'
         })
 
     .when('/aboutus', {
